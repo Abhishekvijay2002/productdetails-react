74b18a2b6a07f0175b70d319479c591399d98df3
abhishek vijayan <bhshkvijayan@gmail.com>
2025-01-31 21:39:39 +0530

    first commit

--- src/assets/ProductDetail.jsx
+++ src/assets/ProductDetail.jsx
@@ -2,7 +2,8 @@ import { useEffect,useState } from "react";
 import Footer from "./footer";
 import axios from "axios";
 import ReactLoading from 'react-loading';
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
 
 function ProductDetail() {
   
@@ -24,15 +25,16 @@ function ProductDetail() {
     </header>
     <div className="container my-4">
 { product.length != 0 ? (<div className="row productdetail-container">
-    <div class="col-sm-6 product-image-container">
+    <div className="col-sm-6 product-image-container">
                     <img src={product?.image} alt=""/>
                 </div>
                 <div className="col-sm-6">
                     <h2>{product?.title}</h2>
                     <p className="text-muted">{product?.category}</p>
                     <p>{product?.description}</p>
-                    <h3 class="text-primary">${product?.price}</h3>
-                    <button class="btn btn-success">Buy Now</button>
+                    <h3 className="text-primary">${product?.price}</h3>
+                    <Button variant="success" size="sm">BUY NOW</Button>
+                    <Link to={`/`}><Button variant="primary mx-1" size="sm">Go Home</Button></Link>
                     </div>
                 </div>):(<div className='d-flex justify-content-center align-items-center loading-container'><ReactLoading type={'spin'} color={`fff`} height={100} width={100} /></div>)}
     
